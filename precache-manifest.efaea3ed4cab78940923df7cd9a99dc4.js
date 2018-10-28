1642c7711d665477956866bfc31b3d5cdc5e6046
Cindy Son <cindyson@mail.com>
2018-10-27 17:55:59 -0700

    Updates

---
+++ precache-manifest.efaea3ed4cab78940923df7cd9a99dc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18d0813118415ca3565b",
+    "url": "/thatgeekgal/react-tutorial/static/js/runtime~main.18d08131.js"
+  },
+  {
+    "revision": "effd9d257b9c58deb66e",
+    "url": "/thatgeekgal/react-tutorial/static/js/main.effd9d25.chunk.js"
+  },
+  {
+    "revision": "a57afc7bde2f4da406d1",
+    "url": "/thatgeekgal/react-tutorial/static/js/1.a57afc7b.chunk.js"
+  },
+  {
+    "revision": "effd9d257b9c58deb66e",
+    "url": "/thatgeekgal/react-tutorial/static/css/main.cd3d0c27.chunk.css"
+  },
+  {
+    "revision": "20a935271bbb8b73fcc31f6de0fb74a2",
+    "url": "/thatgeekgal/react-tutorial/index.html"
+  }
+];
